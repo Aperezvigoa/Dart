efd88fdf357494b9ca662f9f21f8bc7c3a5d835b
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-28 18:09:30 +0200

    Using the while loop

---
+++ 03_Control-Flow-Structures/04_while-loop/while.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  int number = 1;
+
+  while (number < 10) {
+    print('number is still not equal to ten -> $number');
+    number++;
+  }
+
+  print('Number out of the while loop: $number');
+}
