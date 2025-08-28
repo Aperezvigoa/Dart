c5281dc9fa3c2e0d1f1ef477d73110f44d5fd291
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-29 00:19:45 +0200

    Understanding pass by reference in Dart·

---
+++ 04_Functions/04_passing-by-value-and-reference/passing.dart
@@ -0,0 +1,35 @@
+void main() {
+
+  Map<String, String> person = {
+    'name': 'Peter',
+    'username': 'Parker'
+  };
+
+  final Map<String, String> personCap = capitalizeMap(person);
+  print(personCap);
+  print(person);
+
+  // Both were capitalized, because the function gets the memory address from 'person'
+  // How to avoid this? inside the function we can create a copy of the referenced map.
+
+  Map<String, String> person2 = {
+    'name': 'Clark',
+    'username': 'Kent'
+  };
+
+  final Map<String, String> person2Cap = capitalizeCopyMap(person2);
+  print(person2Cap);
+  print(person2);
+}
+
+Map<String, String> capitalizeMap(Map<String, String> person) {
+  person['name'] = person['name']?.toUpperCase() ?? 'Not name detected';
+  return person;
+}
+
+
+Map<String, String> capitalizeCopyMap(Map<String, String> person) {
+  final Map<String, String> copyPerson = {...person};
+  copyPerson['name'] = copyPerson['name']?.toUpperCase() ?? 'Not name detected';
+  return copyPerson;
+}
