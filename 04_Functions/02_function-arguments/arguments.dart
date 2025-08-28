3ed1ae0bab5b7d4e1aab293c44d56bccb47981b5
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-28 23:24:40 +0200

    Using arguments, default arguments & optional arguments

---
+++ 04_Functions/02_function-arguments/arguments.dart
@@ -0,0 +1,34 @@
+void main() {
+
+  sayHi('Alex');
+
+  // Optional arguments
+  greeting();
+  greeting(name:'Cheddar');
+
+  recieveColor('Alex', 'Orange');
+
+  printUserCity('Alex', city: 'Madrid');
+  printUserCity('Jon');
+}
+
+// Simple function that prints a message
+void sayHi(String name) {
+  print('Hello, $name. Nice to meet you!');
+}
+
+// A function with optional arguments
+void greeting({String? name}) {
+  final greetName = name ?? '';
+  print('Congratulations $greetName!');
+}
+
+// More than one argument
+void recieveColor(String name, String color) {
+  print('Wow $name! the color $color is so cool!');
+}
+
+// Default arguments
+void printUserCity(String name, {String city = 'New York'}) {
+  print('The user $name lives in $city');
+}
