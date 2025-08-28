18f69092db83298de0c402da5686935b39953b08
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-28 18:12:19 +0200

    Using the while loop

--- 03_Control-Flow-Structures/04_while-loop/while.dart
+++ 03_Control-Flow-Structures/04_while-loop/while.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
 
   int number = 1;
@@ -8,4 +10,13 @@ void main() {
   }
 
   print('Number out of the while loop: $number');
+
+  String stop = 'n';
+
+  while (stop == 'n') {
+    stdout.writeln('Do you want to stop the loop? y/n');
+    stop = stdin.readLineSync() ?? 'n';
+  }
+
+  print('Loop ended!');
 }
