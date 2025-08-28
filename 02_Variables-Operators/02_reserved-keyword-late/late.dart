816b006bffce11c94cc828fd958c34070244d577
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-08-28 15:13:19 +0200

    Understanding late

--- 02_Variables-Operators/02_reserved-keyword-late/late.dart
+++ 02_Variables-Operators/02_reserved-keyword-late/late.dart
@@ -3,7 +3,7 @@ void main() {
   // In Dart, a late variable is declared without an initial value, but is guaranteed
   // to be initialized before it is accessed. This allows deferred initialization, which
   // can be useful for expensive computations or values that are not immediately
-  // availbale.
+  // availbale. 
 
   double simpleFloat = 10.25;
   print('Simple float: $simpleFloat');
